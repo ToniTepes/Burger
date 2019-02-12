2ee77f0787caf571ca27377c61e070c544aa0b37
Antonio Stacy Foushee <AntonioStacy@gmail.com>
2019-02-12 18:57:14 -0500

    last commit not working ever

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
-var mysql = require("mysql");
-var connection;
+require("dotenv").config();
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'garvey666',
-    database: 'burgers_db'
-  });
-};
-connection.connect();
+const mysql = require("mysql");
+const keys = require("../keys");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3000,
+    user: "root",
+    password: keys.mysql.password,
+    database: "burger_db"
+});
+
+connection.connect(function(err){
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id" + connection.threadId);
+});
+
 module.exports = connection;

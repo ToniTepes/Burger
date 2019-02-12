27488988f07d3ff18b04e6573c26720b888f50d0
Antonio Stacy Foushee <AntonioStacy@gmail.com>
2019-02-12 13:21:11 -0500

    hero2

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,15 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3000,
-  user: "root",
-  password: "garvey666",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'garvey666',
+    database: 'burgers_db'
+  });
+};
+connection.connect();
 module.exports = connection;
